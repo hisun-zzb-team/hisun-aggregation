6e7379f417f4555de3f24aff6ef6a2f3b5938ca2
60514 <www19940913>
2018-05-19 15:38:24 +0800

    提交批量编译模块代码

--- zzb-develop.bat
+++ zzb-develop.bat
@@ -6,18 +6,9 @@ set MVN=mvn
 set MAVEN_OPTS=%MAVEN_OPTS% -Xmx1024m -XX:MaxPermSize=128M
 
 (cd ../hisun-pom && call %MVN% clean install -Dmaven.test.skip=true)
-&& (cd ../hisun-commons && call %MVN% clean install -Dmaven.test.skip=true)
-&& ( cd ../hisun-base && call %MVN% clean install -Dmaven.test.skip=true)
-& (cd ../hisun-sys && call %MVN% clean install -Pdevelop -Dmaven.test.skip=true)
-& (cd ../hisun-zzb && call %MVN% clean install -Dmaven.test.skip=true)
+(cd ../hisun-commons && call %MVN% clean install -Dmaven.test.skip=true)
+(cd ../hisun-base && call %MVN% clean install -Dmaven.test.skip=true)
+(cd ../hisun-sys && call %MVN% clean install -Pdevelop -Dmaven.test.skip=true)
+(cd ../hisun-zzb && call %MVN% clean install -Dmaven.test.skip=true)
+(cd ../hisun-aggregation && call %MVN% clean install -Phisun-zhzg -Dmaven.test.skip=true)
 
-
-cd ../hisun-aggregation
-call %MVN% clean install -Phisun-zhzg -Dmaven.test.skip=true
-if errorlevel 1 goto error
-
-goto end
-:error
-echo Error Happen!!
-:end
-pause
